41af69f8972a3643669c6db00b270bbe1a083962
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2022-05-05 23:34:17 +0300

    lesson6 finish

---
+++ todolistonelove/src/EditSpan.tsx
@@ -0,0 +1,27 @@
+import React, {ChangeEvent, useState} from "react";
+
+type PropsType={
+    title:string
+    callBack:(newTitle:string)=>void
+}
+
+
+export const EditSpan=(props:PropsType)=>{
+    const [editMode, setEditMode]=useState(false)
+    const [newTitle, setNewTitle]=useState(props.title)
+    const onDoubleClickHandler=()=>{
+        setEditMode(!editMode)
+        props.callBack(newTitle)
+    }
+    const onChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+        setNewTitle(e.currentTarget.value)
+    }
+
+    return ( editMode
+            ? <input value={newTitle}
+                     autoFocus
+                     onBlur={onDoubleClickHandler}
+                     onChange={onChangeHandler}/>
+            : <span onDoubleClick={onDoubleClickHandler}>{props.title}</span>
+    )
+}
